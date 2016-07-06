b237404693a2405c2200bd6b772dbe60da482e60
Darren Burgess <darrentburgess@gmail.com>
2016-07-06 09:26:44 -0400

    viewing text files - convert list of files to links using hrefs - point each link to the location of the file - create a route to display the text file - the browser will display the file as plain text - set content-type so browser will display as plain text

--- cms.rb
+++ cms.rb
@@ -3,7 +3,24 @@ require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'pry'
 
+=begin
+viewing text files
+- convert list of files to links using hrefs
+- point each link to the location of the file
+- create a route to display the text file
+- the browser will display the file as plain text
+- set content-type so browser will display as plain text
+=end
+
 get "/" do
   @files = Dir["./data/*"].map { |f| File.basename f }
-  erb :home, layout: :layout 
+  erb :index, layout: :layout 
+end
+
+get "/data/:file_name" do
+  file_name = params[:file_name]
+  file = File.open "data/#{file_name}"
+
+  headers["Content-Type"] = "text/plain"
+  @content = file.read
 end

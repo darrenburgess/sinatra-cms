170166f0023bf723f8be1766f6cd6f64ea38f285
Darren Burgess <darrentburgess@gmail.com>
2016-07-06 08:54:15 -0400

    add an index page - create a public directory - create three text documents: about, changes, history - modify the home route to capture the file names in the public directory - create home.erb file with unordered list of the files - create a layout.erb file with basic html structure

--- cms.rb
+++ cms.rb
@@ -3,6 +3,14 @@ require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'pry'
 
+# plan:
+# - create a public directory
+# - create three text documents: about, changes, history
+# - modify the home route to capture the file names in the public directory
+# - create home.erb file with unordered list of the files
+# - create a layout.erb file with basic html structure
+
 get "/" do
-  "hello, world"
+  @files = Dir["./data/*"].map { |f| File.basename f }
+  erb :home, layout: :layout 
 end

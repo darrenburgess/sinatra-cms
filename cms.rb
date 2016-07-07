5d5a93b31925a581b472318ee09f578fd3d0e7c5
Darren Burgess <darrentburgess@gmail.com>
2016-07-07 11:53:59 -0400

    handle requests for nonexistent documents - add a not_found block that redirects to home - add a check to the file display block to check if the file exists - enable sessions in a configure block   - use a configure block to enable sessions and set a session secret.   - must restart server to get the session to work - modify route to store a flash message on file error in the session hash - in layout.erb, create a conditional paragraph that   displays if there is an error. This should display and delete the session[:error] key - write a test:   - execute the route with a nonexistant file   - confirm the redirect   - confirm the flash message text

--- cms.rb
+++ cms.rb
@@ -4,23 +4,44 @@ require 'tilt/erubis'
 require 'pry'
 
 =begin
-add tests
-- create a cms_test.rb file in new test directory
-- add testing setup code and required libraries
-- create a CmsTest class that subclasses from Minitest::Test 
-- write tests for testing the index route
-- write tests for testing the data/file route
+handle requests for nonexistent documents
+- add a not_found block that redirects to home
+- add a check to the file display block to check if the file exists
+- modify route to store a flash message on file error
+- in layout.erb, create a conditional paragraph that
+  displays if there is an error
+- enable sessions in a configure block
+  - use a configure block to enable sessions and set a session secret.
+  - must restart server to get the session to work
+- write a test:
+  - execute the route with a nonexistant file
+  - confirm the redirect
+  - confirm the flash message text
 =end
 
-get "/" do
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+before do
   @files = Dir["./data/*"].map { |f| File.basename f }
-  erb :index, layout: :layout 
+end
+
+get "/" do
+  erb :index, layout: :layout
 end
 
 get "/data/:file_name" do
   file_name = params[:file_name]
-  file = File.open "data/#{file_name}"
+  full_path = "data/#{file_name}"
 
-  headers["Content-Type"] = "text/plain"
-  @content = file.read
+  if File.exist? full_path
+    file = File.open full_path
+    headers["Content-Type"] = "text/plain"
+    @content = file.read
+  else
+    session[:error] = "#{file_name} does not exist!"
+    redirect "/"
+  end
 end

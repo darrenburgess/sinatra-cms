8d8ed8a3d3f00a2a5e55812d8e9afa71f849a8d0
Darren Burgess <darrentburgess@gmail.com>
2016-07-10 14:44:17 -0400

    refactor tests to use isolated data

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -27,30 +27,40 @@ class CmsTest < Minitest::Test
   end
 
   def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+
     get "/"
+
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.md"
   end
 
   def test_text_data
-    get "/data/changes.txt"
+    create_document "changes.txt", "changes text"
+
+    get "/changes.txt"
+
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, "changes text"
   end
 
   def test_markdown_data
-    get "/data/about.md"
+    create_document "about.md", "# about heading text"
+
+    get "about.md"
+
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "<h1>about heading text</h1>"
   end
 
   def test_nonexistant_file
-    get "/data/garbage_file.testing"
+    get "/garbage_file.testing"
+
     assert_equal 302, last_response.status
 
     get last_response["location"]
@@ -60,21 +70,29 @@ class CmsTest < Minitest::Test
   end
 
   def test_edit_view
-    get "/data/about.md/edit"
+    create_document "about.md", "test content"
+
+    get "/about.md/edit"
+
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "Edit content of: about.md"
-    assert_includes last_response.body, "<textarea"
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, "test content"
   end
 
   def test_file_save
-    post "/data/about.md/save", content: "new content"
+    create_document "about.md", "test content"
+
+    post "/about.md", content: "new content"
+
     assert_equal 302, last_response.status
 
     get last_response["Location"]
-    assert_includes last_response.body, "about.md has been updated"
+    assert_includes last_response.body, "about.md was successfully updated"
 
-    get "/data/about.md"
+    get "/about.md"
+
     assert_equal 200, last_response.status
     assert_includes last_response.body, "new content" 
   end
